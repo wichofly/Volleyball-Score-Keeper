d66ee22cf5afc4397333f671585bbd59b149cbc4
Mauricio Erazo <antonierazo13@hotmail.com>
2022-07-02 10:54:31 +0200

    making the same for team 2
    now i added  a condicition to stop the score to a specific number

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const team2Display = document.querySelector('#team2Display');
 
 
 let team1Score = 0;
+let team2Score = 0;
 let winningScore = 10;
 
 team1Button.addEventListener('click', function () {
-    team1Score += 1; // every click increments one
-    team1Display.textContent = team1Score;
+    if (team1Score !== winningScore) {
+        team1Score += 1; // every click increments one
+        team1Display.textContent = team1Score;
+    }
+})
 
+team2Button.addEventListener('click', function () {
+    if (team2Score !== winningScore) { // if team 2 score is not equal to the winning score 
+        team2Score += 1; // every click increments one
+        team2Display.textContent = team2Score;
+    }
 })
